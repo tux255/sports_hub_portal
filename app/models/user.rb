369f264ff8cbb4b633f557c79a1aaea49a7b3ca0
Andrii Haranin <tux255@gmail.com>
2021-05-26 22:40:33 +0300

    user model updated to has_many :articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :lockable
+
+  has_many :articles
 end

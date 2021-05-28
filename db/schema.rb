1d7271fafc1791cb4f874b849ee1ebf52129e767
Andrii Haranin <tux255@gmail.com>
2021-05-28 09:25:23 +0300

    published:boolean column added for articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_26_190820) do
+ActiveRecord::Schema.define(version: 2021_05_28_061618) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_05_26_190820) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
+    t.boolean "published"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 

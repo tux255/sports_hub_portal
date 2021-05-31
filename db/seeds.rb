ed95be9a35b8e961ec10715861f1761fd19c8e84
Andrii Haranin <tux255@gmail.com>
2021-05-31 11:18:49 +0300

    Seed added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,19 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+10.times do |_index|
+  user_name = Faker::Name.name
+  user_email = Faker::Internet.email
+  u = User.create(name: user_name, password: 'aqwe123', email: user_email)
+
+  rand(10).times do
+    u.articles.create(title: Faker::Lorem.sentence(word_count: 3), body: Faker::Lorem.sentences(number: 2))
+  end
+
+  puts "CREATED USER #{user_email} WITH #{u.articles.count} ARTICLES"
+end

82c41925b00b7e9783d7450d5bb92758851ee43b
Andrii Haranin <tux255@gmail.com>
2021-06-17 13:23:41 +0300

    Post models tests added

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe Post do
+  describe 'post validation test' do
+    before(:each) do
+      @admin = FactoryBot.build(:user, :admin)
+    end
+
+    it 'is valid with valid attributes' do
+      expect(@admin.posts.new(FactoryBot.attributes_for(:post))).to be_valid
+    end
+
+    it 'is not valid without title' do
+      expect(@admin.posts.new(FactoryBot.attributes_for(:post, :with_no_title)))
+        .to_not be_valid
+    end
+
+    it 'is not valid wothout body' do
+      expect(@admin.posts.new(FactoryBot.attributes_for(:post, :with_no_body)))
+        .to_not be_valid
+    end
+  end
+end

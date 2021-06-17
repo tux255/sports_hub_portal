27e41d7d0d34f8e43aa351da0e24a06350648181
Andrii Haranin <tux255@gmail.com>
2021-06-17 15:01:01 +0300

    Survey tests updated

---
+++ spec/factories/survey_answer.rb
@@ -0,0 +1,13 @@
+require 'faker'
+
+FactoryBot.define do
+  factory :survey_answer do
+    answer { Faker::Lorem.sentence(word_count: 2) }
+
+    trait :with_empty_line do
+      answer { '' }
+    end
+
+    association :survey, factory: :survey
+  end
+end

0b998dab391e152267f623115860989a41776c34
Andrii Haranin <tux255@gmail.com>
2021-06-17 13:22:58 +0300

    Survey models test updated

--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -1,43 +1,80 @@
 require 'rails_helper'
 
-# TODO: use FactoryBot for entties here
-
-RSpec.describe Survey, type: :model do
-  it 'is valid with valid attributes' do
-    user = FactoryBot.build(:user, :admin)
-    survey = user.surveys.new(FactoryBot.attributes_for(:survey))
-
-    2.times do
-      survey.survey_answers.new(FactoryBot.attributes_for(:survey_answer))
+RSpec.describe Survey do
+  describe 'survey validation test' do
+    before(:each) do
+      @admin = FactoryBot.build(:user, :admin)
     end
 
-    expect(survey).to be_valid
-  end
+    it 'is valid with valid attributes' do
+      expect(@admin.surveys.new(
+               question: 'Question?',
+               start_date: Date.today,
+               end_date: Date.tomorrow,
+               survey_answers_attributes: {
+                 1 => { answer: 'Yes' },
+                 2 => { answer: 'No' }
+               }
+             )).to be_valid
+    end
 
-  it 'is not valid with less than two answers' do
-    survey = FactoryBot.build(:survey)
-    survey.survey_answers.new(FactoryBot.attributes_for(:survey_answer))
-    expect(survey).to_not be_valid
-  end
+    it 'is not valid with one answer' do
+      expect(@admin.surveys.new(
+               question: 'Question?',
+               start_date: Date.today,
+               end_date: Date.tomorrow,
+               survey_answers_attributes: {
+                 1 => { answer: 'Yes' }
+               }
+             )).to_not be_valid
+    end
 
-  it 'is not valid without a question' do
-    survey = FactoryBot.build(:survey)
+    it 'is not valid without question' do
+      expect(@admin.surveys.new(
+               question: '',
+               start_date: Date.today,
+               end_date: Date.tomorrow,
+               survey_answers_attributes: {
+                 1 => { answer: 'Yes' },
+                 2 => { answer: 'No' }
+               }
+             )).to_not be_valid
+    end
 
-    expect(survey).to_not be_valid
-  end
+    it 'is not valid without a start_date' do
+      expect(@admin.surveys.new(
+               question: 'Question?',
+               start_date: '',
+               end_date: Date.tomorrow,
+               survey_answers_attributes: {
+                 1 => { answer: 'Yes' },
+                 2 => { answer: 'No' }
+               }
+             )).to_not be_valid
+    end
 
-  it 'is not valid without a start_date' do
-    survey = Survey.new(question: 'Is this thing works?', start_date: nil, end_date: Date.tomorrow)
-    expect(survey).to_not be_valid
-  end
+    it 'is not valid without a end_date' do
+      expect(@admin.surveys.new(
+               question: 'Question?',
+               start_date: Date.today,
+               end_date: '',
+               survey_answers_attributes: {
+                 1 => { answer: 'Yes' },
+                 2 => { answer: 'No' }
+               }
+             )).to_not be_valid
+    end
 
-  it 'is not valid without a end_date' do
-    survey = Survey.new(question: 'Is this thing works?', start_date: Date.today, end_date: nil)
-    expect(survey).to_not be_valid
-  end
-
-  it 'is not valid if start_date is in the past' do
-    survey = Survey.new(question: 'Is this thing works?', start_date: Date.yesterday, end_date: Date.tomorrow)
-    expect(survey).to_not be_valid
+    it 'is not valid if start_date is in the past' do
+      expect(@admin.surveys.new(
+               question: 'Question?',
+               start_date: Date.yesterday,
+               end_date: Date.tomorrow,
+               survey_answers_attributes: {
+                 1 => { answer: 'Yes' },
+                 2 => { answer: 'No' }
+               }
+             )).to_not be_valid
+    end
   end
 end

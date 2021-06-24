361d5675e6af5e85b1e4877c94168611a5c4be60
Andrii Haranin <tux255@gmail.com>
2021-06-24 16:18:16 +0300

    schema timestamps

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_22_080708) do
+ActiveRecord::Schema.define(version: 2021_06_24_115931) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -82,6 +82,7 @@ ActiveRecord::Schema.define(version: 2021_06_22_080708) do
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"

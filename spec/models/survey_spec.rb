18860ee8341dc35063683052e88c7a2af1a8f3e8
Andrii Haranin <tux255@gmail.com>
2021-06-25 14:57:18 +0300

    Fixed survey models spec

--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -8,48 +8,48 @@ RSpec.describe Survey do
 
     it 'is valid with valid attributes' do
       expect(
-        FactoryBot.create(:survey)
+        FactoryBot.build(:survey, user_id: user.id)
       ).to be_valid
     end
 
     it 'is not valid with one answer' do
       expect(
-        FactoryBot.create(:survey, :one_answer)
+        FactoryBot.build(:survey, :one_answer, user_id: user.id)
       ).not_to be_valid
     end
 
     it 'is not valid without question' do
       expect(
-        FactoryBot.create(:survey, :no_question)
+        FactoryBot.build(:survey, :no_question, user_id: user.id)
       ).not_to be_valid
     end
 
     it 'is not valid without a start_date' do
       expect(
-        FactoryBot.create(:survey, :no_start_date)
+        FactoryBot.build(:survey, :no_start_date, user_id: user.id)
       ).not_to be_valid
     end
 
     it 'is not valid without a end_date' do
       expect(
-        FactoryBot.create(:survey, :no_end_date)
+        FactoryBot.build(:survey, :no_end_date, user_id: user.id)
       ).not_to be_valid
     end
 
     it 'is not valid if start_date is in the past' do
       expect(
-        FactoryBot.create(:survey, :start_date_in_past)
+        FactoryBot.build(:survey, :start_date_in_past, user_id: user.id)
       ).not_to be_valid
     end
 
     it 'persisted' do
       question = 'what number?'
-      survey = FactoryBot.create(:survey, question: question)
+      survey = FactoryBot.build(:survey, question: question, user_id: user.id)
 
       expect(survey.save).to be_truthy
       expect(survey.user_id).to eq(user.id)
       expect(survey.question).to eq(question)
-      expect(survey.survey_answers.pluck(:answer).sort).to eq(%w[1 2])
+      expect(survey.survey_answers.pluck(:answer)).to eq(%w[Yes No])
     end
   end
 end
